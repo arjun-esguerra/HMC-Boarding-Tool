c4d02e9b7db8153c895326af1fdf2be803c26cf8
arjun-esguerra <arjun.esguerra@hmcarchitects.com>
2024-05-30 16:34:05 -0700

    sacramento phone numbers are paired

--- src/main/java/com/hmc/createhmcuser/Controller.java
+++ src/main/java/com/hmc/createhmcuser/Controller.java
@@ -8,12 +8,14 @@ import javafx.scene.control.ComboBox;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class Controller {
 
@@ -23,7 +25,7 @@ public class Controller {
     @FXML
     private ComboBox<Integer> numberComboBox;
 
-    private Map<String, List<Integer>> officePhoneNumbers;
+    private Map<String, List<String>> officePhoneNumbers;
 
     public void initialize() throws IOException {
         setOfficeComboBox();
@@ -46,16 +48,32 @@ public class Controller {
     public void loadPhoneNumbers() throws IOException {
 
         String jsonContent = new String(Files.readAllBytes(Paths.get("src/main/resources/phone_numbers.json")));
-        System.out.println(jsonContent);
+        JSONObject jsonObject = new JSONObject(jsonContent);
+        JSONArray jsonArray = jsonObject.getJSONArray("TelephoneNumbers");
+
+        officePhoneNumbers = new HashMap<>();
+
+        // loop through the jsonArray and add phone numbers
+        for (int i = 0; i < jsonArray.length(); i++) {
+        JSONObject phoneNumberObject = jsonArray.getJSONObject(i);
+        String phoneNumber = phoneNumberObject.getString("TelephoneNumber");
+        String office = getOffice(phoneNumber);
+
+        // hash phone numbers by office
+        officePhoneNumbers.putIfAbsent(office, new ArrayList<>());
+        officePhoneNumbers.get(office).add(phoneNumber);
+
+        }
+
+        System.out.println(officePhoneNumbers);
     }
 
     public String getOffice(String phoneNumber) {
-        String prefix = phoneNumber.substring(0, 3);
-        switch (prefix) {
-            case "1916":
-                return "Sacramento";
-            default:
-                return "Unknown";
+        String prefix = phoneNumber.substring(0, 4);
+        if (prefix.equals("1916")) {
+            return "Sacramento";
+        } else {
+            return "Unknown";
         }
     }
 
