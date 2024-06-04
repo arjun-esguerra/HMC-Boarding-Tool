0eda4ae5d03d8715fa91c4d5a04b2456a1ada553
arjun-esguerra <arjun.esguerra@hmcarchitects.com>
2024-06-04 10:21:00 -0700

    assigned javafx attributes ids

--- src/main/java/com/hmc/createhmcuser/CSV_Writer.java
+++ src/main/java/com/hmc/createhmcuser/CSV_Writer.java
@@ -1,4 +1,15 @@
 package com.hmc.createhmcuser;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class CSV_Writer {
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
 }
--- src/main/java/com/hmc/createhmcuser/Controller.java
+++ src/main/java/com/hmc/createhmcuser/Controller.java
@@ -82,7 +82,6 @@ public class Controller {
                 numberComboBox.show();
                 numberComboBox.hide();
 
-
             }
         });
 
