37036e8bdf79a6c8720bfa20b5430340a94ce187
arjun-esguerra <arjun.esguerra@hmcarchitects.com>
2024-06-07 13:20:05 -0700

    retrieve available numbers from Teams

--- teamsScript.ps1
+++ teamsScript.ps1
@@ -1,6 +1,22 @@
-Import-Module Teams
+Connect-MicrosoftTeams
+<#
+$unassignedNumbers = Get-CsPhoneNumberAssignment | Where-Object  { $_.LineURI -like $null }
+$json = $unassignedNumbers | Select-Object TelephoneNumber | ConvertTo-Json
 
-$credential = Get-Credential
-Connect-MicrosoftTeams -Credential $credential
+Set-Content -Path .\src\main\resources\phone_numbers.json -Value $json
+#>
 
-Get-Team
+$skip = 0
+$allNumbers = @()
+
+# Loop until Get-CsPhoneNumberAssignment returns no more resutls
+do {
+    $res = Get-CsPhoneNumberAssignment -CapabilitiesContain 'UserAssignment' -NumberType 'CallingPlan' -PstnAssignmentStatus 'Unassigned' -Skip $skip
+    
+    $allNumbers += $res
+
+    $skip += 500
+
+} while ($res)
+
+$allNumbers.TelephoneNumber
