d12b8f67f26eddf505e60475c52dcab9577736c4
arjun-esguerra <arjun.esguerra@hmcarchitects.com>
2024-06-17 14:14:07 -0700

    packaging app

---
+++ src/main/java/com/hmc/createhmcuser/App.java
@@ -0,0 +1,36 @@
+package com.hmc.createhmcuser;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("input_view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Controller controller = fxmlLoader.getController();
+        controller.initialize();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String[] command = {"powershell.exe", "-ExecutionPolicy", "Bypass", "-Command", ". 'src/main/powershell/script.ps1'; getPhoneNumbers"};
+
+        ProcessBuilder pb = new ProcessBuilder(command);
+        Process process = pb.start();
+        process.waitFor();
+
+        launch();
+
+    }
+}
--- src/main/java/com/hmc/createhmcuser/Main.java
+++ src/main/java/com/hmc/createhmcuser/Main.java
@@ -1,36 +1,9 @@
 package com.hmc.createhmcuser;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("input_view.fxml"));
-        Parent root = fxmlLoader.load();
-
-        Controller controller = fxmlLoader.getController();
-        controller.initialize();
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String[] command = {"powershell.exe", "-ExecutionPolicy", "Bypass", "-Command", ". 'src/main/powershell/script.ps1'; getPhoneNumbers"};
-
-        ProcessBuilder pb = new ProcessBuilder(command);
-        Process process = pb.start();
-        process.waitFor();
-
-        launch();
-
+        App.main(args);
     }
 }
